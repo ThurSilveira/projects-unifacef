9231510ea81697e798c882a30c10f957603ce621
ThurSilveira <arthursilveira13000@gmail.com>
2026-03-05 20:36:22 -0300

    final aula Deque

--- estrutura-dados-I/Queue.js
+++ estrutura-dados-I/Queue.js
@@ -1,41 +1,39 @@
-class Queue{
+export default class Queue{
 
-    #data // vetor print
+    #data //Vetor print
 
-    constructor() {
-        this.data = [];
+    constructor(){
+        this.#data = []
     }
 
-    // metodo de inserção na fila
+    //métod de inserção na fila
     enqueue(val){
-        this.#data.push(val);
+        this.#data.push(val)
     }
 
-    // metodo de remoção na fila
+    //métod de remoção na fila
     dequeue(){
-        return this.#data.shift();
+        return this.#data.shift()
     }
 
-    // metodo para consultar o inicio da fila sem remover o elemento
-    peak(){
-        return this.#data[0];
+    //métod para consultar o inicio da fila sem remover o elemento
+    peek(){
+        return this.#data[0]
     }
 
     get isEmpty(){
-        return this.#data.length === 0;
+        return this.#data.length === 0
     }
 
+    //métod para imprimir a fila(efeitos de depuração)
     print(){
-        let output = '[ ';
-        for(let i = 0; i < this.#data.length; i++){
-            if (output !== "[ "){
-                output += ", ";
+        let output = '[ '
+        for(let i=0; i < this.#data.length; i++){
+            if(output !== "[ "){
+                output += ", "
             }
-            output += `(${i}): ${this.#data[i]}`;
+            output += `(${i+1}º): ${this.#data[i]}`
         }
-        return output + " ]";
+        return output + " ]"
     }
-
-}
-
-export default Queue;
+};
